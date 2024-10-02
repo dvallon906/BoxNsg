8e63135f6157b40d2bd6c8b077c8e24dd0408f98
dvallon906 <63501917+dvallon906@users.noreply.github.com>
2024-10-02 19:07:20 +0200

    Correction des erreurs affichée par pyCharm

--- boxNSG.py
+++ boxNSG.py
@@ -41,12 +41,22 @@ def check_maw3():
         # Vérifier si la requête a réussi (code 200)
         if response.status_code == 200:
             # Convertir la réponse JSON en dictionnaire Python
-            data = response.json()
+            #data = response.json()
             #print("Données reçues :", data)
             return True
         else:
             return False
-    except:
+    except requests.exceptions.ConnectionError:
+        print("Erreur: Problème de connexion réseau.")
+        return False
+    except requests.exceptions.Timeout:
+        print("Erreur: La requête a expiré.")
+        return False
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Erreur HTTP: {http_err}")
+        return False
+    except requests.exceptions.RequestException as req_err:
+        print(f"Erreur de requête: {req_err}")
         return False
 
 #**************************************************************************************************
@@ -56,10 +66,10 @@ def check_maw3():
 #   Passe le compteur OffLine à zéro
 # Dans tous les cas d'appel réussi
 #   Incrémente le compteur OnLine de 1
-def checkMaw3EnContinu():
+def verifier_maw3_en_boucle():
     global oStatusLine
 
-    while stop_thread == False:
+    while not stop_thread:
         if check_maw3():
             oStatusLine.modeValidation = 1
             oStatusLine.nbEchecConnexion = 0
@@ -79,7 +89,7 @@ def checkMaw3EnContinu():
 
 
 # Créer un thread pour exécuter la fonction en arrière-plan
-thread = threading.Thread(target=checkMaw3EnContinu)
+thread = threading.Thread(target=verifier_maw3_en_boucle)
 
 # Démarrer le thread
 thread.start()
@@ -98,23 +108,23 @@ thread.start()
 #   Vérification de l'existance d'un user avec ce codeBar dans la base de donnée locale
 #   Si le client n'a pas été trouvé, l'entrée est refusée sauf si le ModeDegradé est autorisé
 #   Dans ce cas, tous les codeBar de 8 caractère permette de valider l'entrée
-def saisirCodeBar(pCodebar:str):
+def saisir_codebar(codebar:str):
     global oStatusLine
     os.system("clear")
     
     # Si le codebar n'a pas été passé en paramêtre à la fonction
-    if pCodebar == "" :
-        pCodebar = (input("Entrez votre codebar :"))
+    if codebar == "" :
+        codebar = (input("Entrez votre codebar :"))
 
     try:
 
         if oStatusLine.modeValidation <= 0:
-            if len(pCodebar) == 8:
+            if len(codebar) == 8:
 
                 # On essaye de trouver le client dans la base de donnée locale
-                if dbTraitements.UsersCodeBarExist(pCodebar) == False:
+                if not dbTraitements.UsersCodeBarExist(codebar):
                     print("Le client n'existe pas dans la base locale")
-                    if oStatusLine.modeDegrade == True:
+                    if oStatusLine.modeDegrade:
                         print("Le système de contrôle d'accès est OffLine, vous pouver rentrer exeptionnelement")
 
                         return True
@@ -133,10 +143,9 @@ def saisirCodeBar(pCodebar:str):
                 return False
             
         # Effectuer une requête GET
-        response = requests.get(oParameters.urlValideCodeBar + pCodebar, timeout=oParameters.timeOutValideCodeBar)
+        response = requests.get(oParameters.urlValideCodeBar + codebar, timeout=oParameters.timeOutValideCodeBar)
 
         # Vérifier si la requête a réussi (code 200)
-    
         if response.status_code == 200:
             # Convertir la réponse JSON en dictionnaire Python
             data = response.json()
@@ -146,13 +155,11 @@ def saisirCodeBar(pCodebar:str):
             prenom = data["prenom"]
 
 
-            if resultat == True:
-                dbTraitements.UsersVersDbLocale(id, pCodebar, nom, prenom)
-
+            if resultat:
+                dbTraitements.UsersVersDbLocale(id, codebar, nom, prenom)
                 print(f"Hello {prenom} {nom}, le système est OnLine, vous pouver entrer")
             else:
                 print("Le système est OnLine mais vos informations n'ont pas été reconnues")
-
 
             return True
         else:
@@ -160,15 +167,23 @@ def saisirCodeBar(pCodebar:str):
             print("Le webservice a renvoyer une réponse invalide:", data)
 
             return False
-    except:
-        data = response.json()
-        print("eLe traitement a levé une exception :", data)
+    except requests.exceptions.ConnectionError:
+        print("Erreur: Problème de connexion réseau.")
+        return False
+    except requests.exceptions.Timeout:
+        print("Erreur: La requête a expiré.")
+        return False
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Erreur HTTP: {http_err}")
+        return False
+    except requests.exceptions.RequestException as req_err:
+        print(f"Erreur de requête: {req_err}")
         return False
 
 # Importe tout les clients qui ont un codeBar selon un centre fourni en paramètre #################
-def UsersImportTousSelonCentre(idCentre):
+def users_import_tous_selon_centre(id_centre):
     # Effectuer une requête GET
-    response = requests.get(oParameters.urlTousSelonCentre + idCentre, timeout=oParameters.timeOutTousSelonCentre)
+    response = requests.get(oParameters.urlTousSelonCentre + id_centre, timeout=oParameters.timeOutTousSelonCentre)
 
     # Vérifier si la requête a réussi (code 200)
     
@@ -176,24 +191,24 @@ def UsersImportTousSelonCentre(idCentre):
         dbTraitements.DeleteAllUsers()
         # Convertir la réponse JSON en dictionnaire Python
         data = response.json()
-        iCompteur = 0   
+        compteur = 0
         for user in data:
             id = user["id"]
-            codeBar = user["codeBar"]
+            codebar = user["codeBar"]
             nom = user["nom"]
             prenom = user["prenom"]
-            dbTraitements.UsersVersDbLocale(id, codeBar, nom, prenom)
-            iCompteur += 1
-        return iCompteur
+            dbTraitements.UsersVersDbLocale(id, codebar, nom, prenom)
+            compteur += 1
+        return compteur
     else:
-        print("Code de retour = " + response.status_code)
+        print("Code de retour = " + str(response.status_code))
         print("date = " + response.json()) 
        
 
 
 #**************************************************************************************************   
 # Affiche à l'écran le temps depuis lequel le sysème est OnLine ou OffLine
-def etatConnexion():
+def etat_connexion():
     os.system("clear")
     global oStatusLine
 
@@ -210,17 +225,17 @@ def main():
         afficher_menu()
         choix = input("Veuillez choisir une option ou saisir un codebar : ")
         if choix == "1":
-            saisirCodeBar("")
+            saisir_codebar("")
         elif choix == "2":
-            etatConnexion()
+            etat_connexion()
         elif choix == "3":
             dbTraitements.DeleteAllUsers()
             os.system("clear")
         elif choix == "4":
-            choixCentre = input("Veuillez choisir le centre à synchroniser : ")
-            iNbUserAjouté = UsersImportTousSelonCentre(choixCentre)
+            choix_centre = input("Veuillez choisir le centre à synchroniser : ")
+            nb_user_ajoute = users_import_tous_selon_centre(choix_centre)
             os.system("clear")
-            print(f"{iNbUserAjouté} users ont été synchronisés")
+            print(f"{nb_user_ajoute} users ont été synchronisés")
         elif choix == "5":
             changer_langue()
         elif choix == "Q":
@@ -230,7 +245,7 @@ def main():
             print("Merci d'avoir utilisé le programme. Au revoir !")
             sys.exit(0)
         else:
-            saisirCodeBar(choix)
+            saisir_codebar(choix)
 
 
 set_language('fr')  # Langue par défaut au démarrage
@@ -239,10 +254,10 @@ os.system("clear")
 # Récupération de l'adresse mac
 gMacAdress = hex(int(uuid.getnode())).split('x')[1]
 
-gMacAdressFormatée = ':'.join([gMacAdress[x:x+2].upper() for x in range(0, len(gMacAdress), 2)])
+gMacAdressFormatee = ':'.join([gMacAdress[x:x+2].upper() for x in range(0, len(gMacAdress), 2)])
 
 print(gMacAdress)
-print(gMacAdressFormatée)
+print(gMacAdressFormatee)
 print(localedir)
 
 # hello()
